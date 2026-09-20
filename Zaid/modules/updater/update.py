1f21ca0238f86fae672f93dc51adbe893ef2044b
hshwghwhs-boop <hshwghwhs@gmail.com>
2026-09-20 23:09:18 +0530

    Update update.py

--- Zaid/modules/updater/update.py
+++ Zaid/modules/updater/update.py
@@ -27,7 +27,7 @@ from config import GIT_TOKEN, REPO_URL, BRANCH
 HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
 
-from Zaid.modules.help import add_command_help
+from Geto.modules.help import add_command_help
 HAPP = None
 
 
