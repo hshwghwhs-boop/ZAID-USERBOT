1c03beeac0010ffb4843c7e2e9aebc6f60eba436
hshwghwhs-boop <hshwghwhs@gmail.com>
2026-09-20 22:55:56 +0530

    Update help.py

--- Zaid/modules/help/help.py
+++ Zaid/modules/help/help.py
@@ -4,9 +4,9 @@ from prettytable import PrettyTable
 from pyrogram import Client, enums, filters
 from pyrogram.types import Message
 
-from Zaid import app, CMD_HELP
-from Zaid.helper.PyroHelpers import ReplyCheck
-from Zaid.helper.utility import split_list
+from Geto import app, CMD_HELP
+from Geto.helper.PyroHelpers import ReplyCheck
+from Geto.helper.utility import split_list
 
 
 async def edit_or_reply(message: Message, *args, **kwargs) -> Message:
