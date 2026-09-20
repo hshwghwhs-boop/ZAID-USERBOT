a6923f33e92344f481626a285fd6fe1d0126b021
hshwghwhs-boop <hshwghwhs@gmail.com>
2026-09-20 22:52:39 +0530

    Update text_api.py

--- Zaid/modules/fun/text_api.py
+++ Zaid/modules/fun/text_api.py
@@ -2,9 +2,9 @@ from aiohttp.client_exceptions import ClientError
 from pyrogram import filters, Client 
 from pyrogram.types import Message
 
-from Zaid import SUDO_USER
-from Zaid.helper.aiohttp_helper import AioHttp
-from Zaid.modules.help import add_command_help
+from Geto import SUDO_USER
+from Geto.helper.aiohttp_helper import AioHttp
+from Geto.modules.help import add_command_help
 
 text_apis_data = {
     "compliment": {
