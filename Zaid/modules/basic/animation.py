ab0213052fa290d097045692c6aa84f52db1d11c
hshwghwhs-boop <hshwghwhs@gmail.com>
2026-09-20 21:26:18 +0530

    Update animation.py

--- Zaid/modules/basic/animation.py
+++ Zaid/modules/basic/animation.py
@@ -9,10 +9,10 @@ from pyrogram.types import *
 from pyrogram.types import Message
 
 
-from Zaid.helper.basic import edit_or_reply, get_text
-from Zaid.helper.constants import MEMES
+from Geto.helper.basic import edit_or_reply, get_text
+from Geto.helper.constants import MEMES
 
-from Zaid.modules.help import *
+from Geto.modules.help import *
 
 DEFAULTUSER = "Man"
 
