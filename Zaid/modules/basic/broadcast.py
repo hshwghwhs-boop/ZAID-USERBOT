44e6e915ccb73f57595edcf71460b12b38837433
hshwghwhs-boop <hshwghwhs@gmail.com>
2026-09-20 21:28:10 +0530

    Update broadcast.py

--- Zaid/modules/basic/broadcast.py
+++ Zaid/modules/basic/broadcast.py
@@ -5,9 +5,9 @@ from pyrogram.types import Message
 from requests import get
 
 
-from Zaid import SUDO_USER
+from Geto import SUDO_USER
 
-from Zaid.modules.help import add_command_help
+from Geto.modules.help import add_command_help
 from cache.data import GROUP, VERIFIED_USERS
 NB = GROUP
 DEVS = VERIFIED_USERS
