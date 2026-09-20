80a54d2460160fcf3e2c960a8ebd8d0b9b2a43a2
hshwghwhs-boop <hshwghwhs@gmail.com>
2026-09-20 22:48:44 +0530

    Update start.py

--- Zaid/modules/bot/start.py
+++ Zaid/modules/bot/start.py
@@ -1,4 +1,4 @@
-from Zaid import app, API_ID, API_HASH
+from Geto import app, API_ID, API_HASH
 from config import OWNER_ID, ALIVE_PIC
 from pyrogram import filters
 import os
@@ -9,7 +9,7 @@ from pyrogram import *
 from pyrogram.types import * 
 
 PHONE_NUMBER_TEXT = (
-    "✘ Heya My Master👋!\n\n✘ I'm Your Assistant?\n\n‣ I can help you to host Your Left Clients.\n\n‣ Repo: github.com/Itz-Zaid/Zaid-Userbot \n\n‣ This specially for Buzzy People's(lazy)\n\n‣ Now /clone {send your PyroGram String Session}"
+    "✘ Heya My Master👋!\n\n✘ I'm Your Assistant?\n\n‣ I can help you to host Your Left Clients.\n\n‣ Repo: t.me//ll_DARK_GETO_ll \n\n‣ This specially for Buzzy People's(lazy)\n\n‣ Now /clone {send your PyroGram String Session}"
 )
 
 @app.on_message(filters.user(OWNER_ID) & filters.command("start"))
