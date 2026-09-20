5c1962c35c02464b33a4329f41d36ae3a63efee0
hshwghwhs-boop <hshwghwhs@gmail.com>
2026-09-20 22:51:23 +0530

    Update import path for add_command_help

--- Zaid/modules/fun/emoji.py
+++ Zaid/modules/fun/emoji.py
@@ -6,7 +6,7 @@ from pyrogram import filters, Client
 from pyrogram.types import Message
 
 
-from Zaid.modules.help import add_command_help
+from Geto.modules.help import add_command_help
 
 emojis = {
     "moon": list("🌗🌘🌑🌒🌓🌔🌕🌖"),
