28bea6cd194d3c5bec454e46b6c4e3c05328b082
hshwghwhs-boop <hshwghwhs@gmail.com>
2026-09-20 22:47:10 +0530

    Update inline.py

--- Zaid/modules/bot/inline.py
+++ Zaid/modules/bot/inline.py
@@ -18,9 +18,9 @@ from pyrogram.types import (
     Message,
 )
 
-from Zaid import CMD_HELP, StartTime, app
-from Zaid.helper.data import Data
-from Zaid.helper.inline import inline_wrapper, paginate_help
+from Geto import CMD_HELP, StartTime, app
+from Geto.helper.data import Data
+from Geto.helper.inline import inline_wrapper, paginate_help
 
 async def get_readable_time(seconds: int) -> str:
     count = 0
