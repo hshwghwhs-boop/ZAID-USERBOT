8d58cc09efd3b1afabde4f25a1f0540410f4a2db
hshwghwhs-boop <hshwghwhs@gmail.com>
2026-09-20 21:34:33 +0530

    Update google.py

--- Zaid/modules/basic/google.py
+++ Zaid/modules/basic/google.py
@@ -4,9 +4,9 @@ from googlesearch import search
 from pyrogram import Client, filters
 from pyrogram.types import Message
 
-from Zaid.helper.basic import edit_or_reply
+from Geto.helper.basic import edit_or_reply
 
-from Zaid.modules.help import *
+from Geto.modules.help import *
 
 
 def googlesearch(query):
