f30c7f39f2465c2a3d0400241677d623201566b6
hshwghwhs-boop <hshwghwhs@gmail.com>
2026-09-20 22:53:25 +0530

    Update texts.py

--- Zaid/modules/fun/texts.py
+++ Zaid/modules/fun/texts.py
@@ -6,11 +6,11 @@ import uwuify
 from pyrogram import filters, Client
 from pyrogram.types import Message
 
-from Zaid.helper.PyroHelpers import GetUserMentionable
-from Zaid.helper.aiohttp_helper import AioHttp
-from Zaid.helper.constants import Fs, MEMES, Weebify
-from Zaid.helper.utility import get_mock_text
-from Zaid.modules.help import add_command_help
+from Geto.helper.PyroHelpers import GetUserMentionable
+from Geto.helper.aiohttp_helper import AioHttp
+from Geto.helper.constants import Fs, MEMES, Weebify
+from Geto.helper.utility import get_mock_text
+from Geto.modules.help import add_command_help
 
 
 @Client.on_message(filters.command("lorem", ".") & filters.me)
