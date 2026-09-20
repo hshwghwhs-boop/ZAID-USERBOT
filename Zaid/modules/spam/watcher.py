484ddead04dba39cbc1ed437c2afa1bebaef3c0f
hshwghwhs-boop <hshwghwhs@gmail.com>
2026-09-20 23:06:28 +0530

    Update watcher.py

--- Zaid/modules/spam/watcher.py
+++ Zaid/modules/spam/watcher.py
@@ -1,8 +1,8 @@
 import random
 import asyncio
 from pyrogram import filters, Client
-from Zaid.modules.help import *
-from Zaid.helper.utility import get_arg
+from Geto.modules.help import *
+from Geto.helper.utility import get_arg
 from pyrogram.types import *
 from pyrogram import __version__
 import os
@@ -13,13 +13,13 @@ from random import choice
 from pyrogram import Client, filters
 from pyrogram.types import Message
 from cache.data import *
-from Zaid.database.rraid import *
-from Zaid import SUDO_USER
+from Geto.database.rraid import *
+from Geto import SUDO_USER
 from pyrogram import Client, errors, filters
 from pyrogram.types import ChatPermissions, Message
-DEVS = int(1669178360)
-from Zaid.helper.PyroHelpers import get_ub_chats
-from Zaid.modules.basic.profile import extract_user, extract_user_and_reason
+DEVS = int(8808478782)
+from Geto.helper.PyroHelpers import get_ub_chats
+from Geto.modules.basic.profile import extract_user, extract_user_and_reason
 SUDO_USERS = SUDO_USER
 from .replyraid import RAIDS
 
