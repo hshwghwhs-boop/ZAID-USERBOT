b4978f1863f7e871d251b76e05e4f49a73ea7827
hshwghwhs-boop <hshwghwhs@gmail.com>
2026-09-20 21:30:40 +0530

    Update clone.py

--- Zaid/modules/basic/clone.py
+++ Zaid/modules/basic/clone.py
@@ -4,9 +4,9 @@ from pyrogram import *
 from pyrogram.types import *
 
 
-from Zaid.helper.basic import edit_or_reply, get_text, get_user
+from Geto.helper.basic import edit_or_reply, get_text, get_user
 
-from Zaid.modules.help import *
+from Geto.modules.help import *
 
 OWNER = os.environ.get("OWNER", None)
 BIO = os.environ.get("BIO", "404 : Bio Lost")
